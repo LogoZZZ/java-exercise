ee733aecbf4fe639c0d56046e8a86d19b0b35ed9
DogRex <827814990@qq.com>
2018-02-20 13:15:54 +0800

    【测试·基础篇】求和函数

---
+++ src/main/java/com/again/test/jexercise/basic/Sum.java
@@ -0,0 +1,27 @@
+package com.again.test.jexercise.basic;
+
+/**
+ * Created by dogrex on 2018/2/20.
+ * <p>
+ * 描述：给定一个正整数n，给出从1-n之间数字的和<p>
+ *
+ * 示例：<p>
+ * 输入：10 <br>
+ * 输出：55（1+2+3...+10）<br>
+ *
+ */
+public class Sum {
+    public static void main(String[] args){
+        int sum10 = sum(10);
+        int sum100 = sum(100);
+
+        assert sum10 == 55;
+        assert sum100 == 5050;
+    }
+
+    public static int sum(int n)
+    {
+        //TODO 编写实现方法
+        return -1;
+    }
+}
